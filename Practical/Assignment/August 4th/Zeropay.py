6e0317c323895097920acefe8d57fe952d39aea9
Jinseong Choi <jsc.tiding1@gmail.com>
2024-09-10 13:53:18 +0900

    updated README

--- Practical/Assignment/August 4th/Zeropay.py
+++ Practical/Assignment/August 4th/Zeropay.py
@@ -1,24 +1,28 @@
+
+# 필요한 패키지 불러오기
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.webdriver.edge.options import Options as EdgeOptions
-from bs4 import BeautifulSoup
-import time
-import re
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from geopy.geocoders import Nominatim
+
+from bs4 import BeautifulSoup
+from matplotlib import font_manager, rc
+from IPython.display import display, HTML
+from collections import Counter
+
+import time
+import re
 import pymysql
 import pandas as pd
 import numpy as np
 import platform
-from matplotlib import font_manager, rc
-import folium, json
-from IPython.display import display, HTML
-from collections import Counter
-# Import necessary modules
 import matplotlib.pyplot as plt
-# Define helper functions
+import folium, json
+
+# 주소 추출 함수
 def extract_address(addr):
     match = re.search(r'(서울특별시 강동구.*?)(?:\s+02|\s+010|\s+070|\s+0|$)', addr)
     if match:
@@ -27,17 +31,19 @@ def extract_address(addr):
         return address
     return None
 
+# 가게 이름 추출 함수
 def extract_name(addr):
     match = re.search(r'(?:점업|점)\s*(.*?)\s*서울특별시 강동구', addr)
     return match.group(1).strip() if match else None
 
-# Create a class for Selenium scraping
+# Selenium 사용을 위한 함수 클래스 정의
 class SeleniumScraper:
     def __init__(self, driver_path):
         self.driver_path = driver_path
         self.addr_s = []
         self.driver = None
 
+    # 웹페이지 크롤링 기능
     def scrape_page(self, url):
         edge_service = EdgeService(executable_path=self.driver_path)
         edge_options = EdgeOptions()
@@ -54,50 +60,50 @@ class SeleniumScraper:
     def quit_driver(self):
         self.driver.quit()
 
-# Set the path to the Edge WebDriver executable
+# 마이크로소프트 엣지 웹드라이버 경로 정의 
 driver_path = 'C:/msedgedriver.exe'
 
-# Create an instance of SeleniumScraper
+# Selenium을 통해 제로페이 사용처 홈페이지 주소를 읽어들인다
 scraper = SeleniumScraper(driver_path)
 url_to_scrape = "https://zeropay.or.kr/UI_HP_009_03.act"  
 scraper.scrape_page(url_to_scrape)
 scraper.quit_driver()
 
-# Get addr_k from formatted_address.txt
+# 정형화된 주소가 저장된 txt 파일에서 사용 가능한 주소를 추출한다 
 addr_k = []
 addr_s = []
 with open('formatted_address.txt', 'r') as f:
     addr_k = f.readlines()
 
-# Remove unnecessary columns from couponData
+# couponData에서 불필요한 데이터를 삭제하는 절차
 couponData = pd.read_excel('CouponUsage.xlsx', header=2)
 couponData = couponData.drop(couponData.columns[[1, 2, 3]], axis=1)
 couponData = couponData.rename(columns={couponData.columns[1]: '사용처'})
 couponData = couponData.drop(columns=['식권'])
-
-# Rename columns in couponData
 couponData = couponData.rename(columns={couponData.columns[1]: '사용처'})
 
-# Create tables in MySQL
+# MySQL에 Python을 사용해 연결하는 절차
 conn = pymysql.connect(host='localhost', user='root', password='Blame1-Chastise8-Turmoil1-Pointer7', db='zeropay', charset='utf8')
 cur = conn.cursor()
+
+# Python을 통해 MySQL 명령어를 입력한다
 cur.execute("USE zeropay")
 cur.execute("DROP TABLE IF EXISTS zeropay")
 cur.execute("CREATE TABLE zeropay (name VARCHAR(25), address VARCHAR(50), menu VARCHAR(10), gps_location VARCHAR(45))")
 cur.execute("DROP TABLE IF EXISTS payUsage")
 cur.execute("CREATE TABLE payUsage (사용일시 CHAR(9), 사용처 VARCHAR(15), 업종 VARCHAR(7))")
 
-# Insert data into payUsage table
+# MySQL의 couponData 테이블을 업데이트한다
 for i in range(len(couponData)):
     sql = "INSERT INTO payUsage (사용일시, 사용처, 업종) VALUES (%s, %s, %s)"
     cur.execute(sql, (couponData['사용일시'][i], couponData['사용처'][i], couponData['가맹점업종'][i]))
 
-# Commit changes and close connection
+# 변경사항을 확인하고 연결을 종료 
 conn.commit()
 cur.close()
 conn.close()
 
-# Insert data into zeropay table
+# MySQL의 zeropay 테이블을 Python을 사용해 업데이트한다
 for i in range(len(extracted_names)):
     name = extracted_names[i]
     address = extracted_addresses[i]
@@ -108,10 +114,10 @@ for i in range(len(extracted_names)):
     sql = "INSERT INTO zeropay (name, address, menu, gps_location) VALUES (%s, %s, %s, %s)"
     cur.execute(sql, (name, address, menu, gps_location_str))
 
-# Close connection
+# 연결 종료
 conn.close()
 
-# Create a map
+# json 파일을 불러들여 folium으로 강동구 강일동 지도를 생성
 maps = []
 geo_path = 'skorea-provinces-geo.json'
 geo_str = json.load(open(geo_path, encoding='utf-8'))
@@ -124,8 +130,6 @@ map1 = folium.Map(
     zoomControl=False
 )
 
-# Add markers to the map
+# 지도에 마커를 추가하고 표시
 folium.Marker([37.55772265, 127.17013912394845], popup='고덕센트럴푸르지오', icon=folium.Icon(color='red')).add_to(map1)
-
-# Display the map
 display(map1)
--- README.md
+++ README.md
@@ -1,17 +1,39 @@
-#### Engineering-Cupcakes
-===============
+# Project Title
 
-Repository for practicing purpose
+Last one was an Engineering Pancake, and now we show you an Engineering Cupcake.  
+지난 번 팬케이크에 이어서, 이번에는 컵케이크를 선보입니다.
 
-Features
---------
+## Getting Started
 
-* TODO
+This Repository will demonstrate you a few handmaded Python codes. 
+이 레포지토리 안에는 손수 작성한 Python 기반의 코드가 몇 개 들어 있습니다. 
 
-Credits
--------
+With these codes, we expect to show you our efforts and skills.
+이 코드들을 통해, 우리의 노력과 기술을 선보일 수 있기를 기대합니다. 
 
-This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+### Prerequisites
 
-.. _Cookiecutter: https://github.com/audreyr/cookiecutter
-.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+Codes in this repository needs the following environments for correct function.
+이 레포지토리 안의 코드들의 정상 동작을 위해선 다음과 같은 환경이 필요합니다. 
+
+* Python 3.10
+* Matplotlib 3.5.3
+* OpenCV-Python 4.10
+* Flask 2.2.5
+
+Otherwise, the code might not work properly. 
+이와 같지 않을 경우, 코드가 작동하지 않을 수 있습니다. 
+
+## Usage
+
+These codes containing below aspects:
+레포지토리 안의 코드들은 다음과 같은 요소를 포함합니다:
+
+* Face Recognition / 얼굴 인식
+* Object Detection / 객체 인식
+* Photo Adjustment / 사진 보정 
+
+## Assginment
+
+These codes might contain assignments provided from Seoul Software Academy. 
+코드들 중 일부는 서울 소프트웨어 아카데미(SeSAc)에서 요구한 과제 제출에 사용되었습니다. 
