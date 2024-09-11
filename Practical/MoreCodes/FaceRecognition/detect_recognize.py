34c77ecd174c671467c3d011d4fc8c7a5d749ee6
Jsc.ti <pockytalks@proton.me>
2024-09-11 10:07:44 +0900

    made comments on files

--- Practical/MoreCodes/FaceRecognition/detect_recognize.py
+++ Practical/MoreCodes/FaceRecognition/detect_recognize.py
@@ -15,15 +15,16 @@ import time
 from sklearn.svm import SVC
 from sklearn.preprocessing import LabelEncoder
 
+# dlib 얼굴인식 모델 불러오기 
 face_detector = dlib.get_frontal_face_detector()
 face_encoder = dlib.face_recognition_model_v1('./model/dlib_face_recognition_resnet_model_v1.dat')
 face_pose_predictor = dlib.shape_predictor('./model/shape_predictor_68_face_landmarks.dat')
 
 def get_detected_faces(filename):
     """
-    Detect faces in a picture using HOG
-    :param filename: picture filename
-    :return: picture numpy array, face detector object with detected faces
+    HOG(방향성 그래디언트 히스토그램)를 이용하여 사진에서 얼굴을 인식 
+    :입력(filename): 사진 파일의 이름 
+    :출력: numpy 배열, face_detector 함수 동작
     """
     image = cv2.imread(filename)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -31,10 +32,10 @@ def get_detected_faces(filename):
 
 def get_face_encoding(image, detected_face):
     """
-    Encode face into 128 measurements using a neural net
-    :param image: picture numpy array
-    :param detected_face: face detector object with one detected face
-    :return: measurement (128,) numpy array 
+    신경망을 사용하여 얼굴을 암호화
+    :입력 1(image): numpy 배열
+    :입력 2(detected_face): 사진에서 포착된 얼굴 하나
+    :출력: measurement, numpy 배열
     """
     pose_landmarks = face_pose_predictor(image, detected_face)
     face_encoding = face_encoder.compute_face_descriptor(image, pose_landmarks, 1)
@@ -42,11 +43,11 @@ def get_face_encoding(image, detected_face):
 
 def training(people):
     """
-    Training our classifier (Linear SVC). Saving model using pickle.
-    We need to have only one person/face per picture.
-    :param people: people to classify and recognize
+    Linear SVC를 사용해 훈련하는 과정. Pickle을 사용해 만들어진 모델을 저장. 
+    사진 하나 당 한 명(개)의 사람(얼굴)을 포착할 수 있도록 함. 
+    :입력(people): 분류하고 탐지할 사람 개체
     """
-    # parsing labels and reprensations
+    # 주어진 라벨 분석
     df = pd.DataFrame()
     for p in people:
         l = []
@@ -58,18 +59,18 @@ def training(people):
         df = pd.concat([df, temp])
     df.reset_index(drop=True, inplace=True)
 
-    # converting labels into int
+    # 라벨을 int 값으로 변환하는 과정
     le = LabelEncoder()
     y = le.fit_transform(df[128])
     print("Training for {} classes.".format(len(le.classes_)))
     X = df.drop(128, axis=1)
     print("Training with {} pictures.".format(len(X)))
 
-    # training
+    # 모델 훈련
     clf = SVC(C=1, kernel='linear', probability=True)
     clf.fit(X, y)
 
-    # dumping model
+    # 만들어진 모델 내보내기
     fName = "./classifier.pkl"
     print("Saving classifier to '{}'".format(fName))
     with open(fName, 'wb') as f:
@@ -77,31 +78,35 @@ def training(people):
 
 def predict(filename, le=None, clf=None, verbose=False):
     """
-    Detect and recognize a face using a trained classifier.
-    :param filename: picture filename
-    :param le:
-    :paral clf:
-    :param verbose:
-    :return: picture with bounding boxes and prediction
+    주어진 모델을 통해 사진에서 사람 얼굴을 탐지하는 기능
+    :입력(filename): 얼굴을 탐지할 사진
+    :입력(le):
+    :입력(clf):
+    :입력(verbose):
+    :출력: 얼굴을 탐지하여 박스로 표기한 사진 
     """
     if not le and not clf:
         with open("./classifier.pkl", 'rb') as f:
             (le, clf) = pickle.load(f)
     image, detected_faces = get_detected_faces(filename)
     prediction = []
-    # Verbose for debugging
+
+    # 디버깅용 장황성 설정
     if verbose:
         print('{} faces detected.'.format(len(detected_faces)))
     img = np.copy(image)
     font = cv2.FONT_HERSHEY_SIMPLEX
+
     for face_detect in detected_faces:
-        # draw bounding boxes
+        # 얼굴 표시용 박스 설정 
         cv2.rectangle(img, (face_detect.left(), face_detect.top()), 
                           (face_detect.right(), face_detect.bottom()), (255, 0, 0), 2)
         start_time = time.time()
-        # predict each face
+
+        # 얼굴별 탐지 
         p = clf.predict_proba(get_face_encoding(image, face_detect).reshape(1, 128))
-        # throwing away prediction with low confidence
+
+        # "자신감"이 상대적으로 떨어지는 탐지 결과는 출력하지 않는다
         a = np.sort(p[0])[::-1]
         if a[0]-a[1] > 0.5:
             y_pred = le.inverse_transform(np.argmax(p))
@@ -109,7 +114,8 @@ def predict(filename, le=None, clf=None, verbose=False):
                           (face_detect.right(), face_detect.bottom())])
         else:
             y_pred = 'unknown'
-        # Verbose for debugging
+
+        # 디버깅용 장황성 설정
         if verbose:
             print('\n'.join(['%s : %.3f' % (k[0], k[1]) for k in list(zip(map(le.inverse_transform, 
                                                                               np.argsort(p[0])), 
@@ -119,21 +125,26 @@ def predict(filename, le=None, clf=None, verbose=False):
         cv2.putText(img, y_pred, (face_detect.left(), face_detect.top()-5), font, np.max(img.shape[:2])/1800, (255, 0, 0))
     return img, prediction
 
+# 메인 함수
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('mode', type=str, help='train or predict')
+    # parser 호출 때 사용할 수 있는 인자값 추가
+    parser.add_argument('mode', type=str, help='훈련인지 탐지인지 결정합니다.')
     parser.add_argument('--training_data',
                         type=str,
-                        help="Path to training data folder.",
+                        help="훈련 데이터 경로를 나타냅니다.",
                         default='./data/')
     parser.add_argument('--testing_data',
                         type=str,
-                        help="Path to test data folder.",
+                        help="테스트 데이터 경로를 나타냅니다.",
                         default='./test/')
 
     args = parser.parse_args()
     people = os.listdir(args.training_data)
-    print('{} people will be classified.'.format(len(people)))
+    # 탐지될 사람의 수 출력
+    print('{}명의 사람이 탐지됩니다.'.format(len(people)))
+
+    # 함수 모드에 따라 동작을 다르게 수행
     if args.mode == 'train':
         training(people)
     elif args.mode == 'test':
--- Practical/MoreCodes/Sentiment Analysis/senti.py
+++ Practical/MoreCodes/Sentiment Analysis/senti.py
@@ -1,31 +1,25 @@
-## analysing text, to predict whether the rest review is positive or not
-
-#supervised learning..
-
-# import numpy as np
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-# import streamlit as st
-#importing dataset
+import streamlit as st
+
+# 데이터셋 불러오기
 dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 
-#Cleaning the texts .. remove stopwords etc, stemming.. similar words like love or loved
-# makes all in small letters
-# sparse matrix
-#Bag of words basis
+## -----데이터셋 청소 단계 
+# 첫 번째 기록에 한정해서 단어 청소 실행
+import re  
+review = re.sub('[^a-zA-Z]',' ',dataset['Review'][0]) # 알파벳 (A부터 Z까지) 제외한 단어를 모두 소거 
+review = review.lower()  # 남은 모든 단어를 소문자로 일괄 변환
 
-#First cleaning is applied to the first record only
-import re   #library used in cleaning
-review = re.sub('[^a-zA-Z]',' ',dataset['Review'][0]) #.. removing letters other than a to z
-review = review.lower()  # at this stage Review is availble in the form of a string
-#remove non significant words
+# 리뷰에서 중요하지 않은 단어를 제거
 import nltk
-# nltk.download('stopwords')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
-review = review.split()  # the split finction converts the string into list
+review = review.split()  # 분석할 문장을 쪼개 단어들의 리스트로 변환
 review = [word for word in review if not word in set(stopwords.words('english'))]
 
-#Stemming .. replace words which are similar like love and loved to the root word like love
+# 과거형이나 미래형 등 시제나 용법이 다른 단어들을 원형으로 변환
 from nltk.stem.porter import PorterStemmer
 ps = PorterStemmer()
 ml = []
@@ -34,16 +28,13 @@ for word in review:
     ml.append(st)
 review = ml
 
-#joining the words to make a string of cleaned words
-review = ' '.join(review) # list is converted back to string
-    
-
+# '다듬어잔' 단어들을 결합하여 String으로 합친다 
+review = ' '.join(review) 
 corpus = []
-corpus.append(review) # string is put back into a list (as a combined string)
-# print(len(corpus))
-##---------------------------------
-#Now the above cleaning is applied to all the remaning records from index 1 to 999
+corpus.append(review)
+# 디버깅 시 "print(len(corpus))" 구문을 사용 가능 
 
+# 이상의 과정을 보유 중인 리뷰 세트 전체에 적용할 필요가 있다
 for i in range(1,1000):
     review1 = re.sub('[^a-zA-Z]',' ',dataset['Review'][i])
     review1 = review1.lower()
@@ -56,60 +47,53 @@ for i in range(1,1000):
         review1 = ml1
     review1 = ' '.join(review1)
     corpus.append(review1)
-##-----------------------------------
-#  To create a bag of words model
-#  it is same as creating a sparse matrix through the process of tokenisation
-# ie to create a separate column for each of the word
-# so finally what we get is a review, column for each word and its frequency
-# The sparse matrix would essentially contain all the required features / feature matrix
-#   Once we get the above bag of words, we shud be apply the classification template
-# this process is handled through a class called as countvectorizer
 
+## -----------------------------------
+#  "단어 가방" 모델 생성 
+# 토큰화 과정을 통해 희소 행렬을 생성하
+# 각 단어별 등장 빈도를 체크, 계산한다
 
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
 X10 = cv.fit_transform(corpus)
-# print(cv.get_feature_names())
 X1 = X10.toarray()
-
 y = dataset.iloc[:, 1].values
+
 ##-----------------------------------
-# Splitting the dataset into the Training set and Test set
+# 데이터 세트를 훈련용과 시험용으로 분리
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X1, y, test_size = 0.20, random_state = 0)
 
-# Fitting Naive Bayes to the Training set
+# 훈련용 세트는 나이브-베이즈 분류 적용 
 from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
-# plt.hist(X_train)
+plt.hist(X_train)
 plt.hist(y_train, label='positive (1) and negative (0)')
 plt.legend()
 plt.show()
 
-
-# Predicting the Test set results
+# 시험용 세트를 예상
 y_pred = classifier.predict(X_test)
 
-# Making the Confusion Matrix
-
+# 혼동 행렬 생성 
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
-print('Confusion Matrix: \n', cm)
+
+# 정확도 계산
+print('혼동 행렬 출력: \n', cm)
 acc_cm =(cm[0,0]+cm[1,1])/(cm[0,0]+cm[0,1]+cm[1,0]+cm[1,1])
-print('Accuracy of the model based on confusion matrix: ', acc_cm)
+print('혼동 행렬 기반 정확도: ', acc_cm)
 acc_score1 = accuracy_score(y_test, y_pred)
-print('Accuracy score of the model: ', acc_score1)
-
-
+print('모델 정확도: ', acc_score1)
 
 #- ------------------------------------------------------------
-#classifying the sehensence entered by the user based on the above model
+# 만들어진 모델에 입각하여 유저로부터 새로 입력을 받는다 (과정은 동일함)
 
 userinput = input("Enter your input :     ")
 # userinput = st.text_area("Enter your input :     ")
 
-review2 = re.sub('[^a-zA-Z]',' ',userinput) #.. removing letters other than a to z
+review2 = re.sub('[^a-zA-Z]',' ',userinput) # 알파벳 이외의 글자를 소거
 review2 = review2.lower()
 
 review2 = review2.split()
@@ -120,16 +104,13 @@ for word in review2:
     st13 = ps.stem(word)
     ml13.append(st13)
     review2 = ml13
-
-#joining the words to make a string of cleaned words
 review2 = ' '.join(review2)
 
 import copy
 corpus_copy = copy.deepcopy(corpus)
 corpus_copy.append(review2)
 
-#It is necessary to put the new record below the existing records so that the new record is also
-#arranged in the same manner..
+# 유저 입력을 분석한 기록을 기존의 기록 아래에 덧붙인다
 
 cv = CountVectorizer(max_features = 1500)
 X111 = cv.fit_transform(corpus_copy).toarray()
@@ -138,8 +119,8 @@ y_pred1 = classifier.predict(X111[-1:][:])
 print(y_pred1)
 
 if y_pred1 == 1:
-    print("The customer feedback is positive")
+    print("긍정적인 피드백입니다.")
 else:
-    print("The customer feedback is negative")
+    print("부정적인 피드백입니다.")
 
 corpus_copy = []
